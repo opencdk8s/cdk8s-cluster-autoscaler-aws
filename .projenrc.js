2ed094b7adeeee0ba51330b814c07b2ef8369a06
Bharat Parmar <bharat.parmar@smallcase.com>
2021-08-27 18:46:24 +0530

    fix: upgrade projen and cdk version

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,11 @@
 const { ConstructLibraryCdk8s } = require('projen');
 
 const CDK_VERSION = '1.113.0';
-
 const project = new ConstructLibraryCdk8s({
   author: 'Hunter Thompson',
   authorAddress: 'aatman@auroville.org.in',
   cdk8sVersion: '1.0.0-beta.10',
+  cdk8sVersionPinning: true,
   defaultReleaseBranch: 'development',
   stability: 'experimental',
   jsiiFqn: 'projen.ConstructLibraryCdk8s',
@@ -18,12 +18,10 @@ const project = new ConstructLibraryCdk8s({
     module: 'cdk8s_cluster_autoscaler_aws',
   },
   peerDeps: [
-    'constructs@^3.3.65',
     `@aws-cdk/aws-iam@${CDK_VERSION}`,
     `@aws-cdk/core@${CDK_VERSION}`,
   ],
   devDeps: [
-    'constructs@^3.3.65',
     `@aws-cdk/aws-iam@${CDK_VERSION}`,
     `@aws-cdk/core@${CDK_VERSION}`,
   ],
@@ -31,14 +29,13 @@ const project = new ConstructLibraryCdk8s({
   dependabot: false,
   gitignore: ['package.json', 'test/'],
   pullRequestTemplate: false,
-  releaseBranches: ['development'],
   codeCov: true,
   clobber: false,
   readme: true,
   mergify: true,
 });
 
-const common_exclude = ['cdk.out', 'package.json', 'yarn-error.log', 'coverage', '.DS_Store', '.idea', '.vs_code'];
+const common_exclude = ['cdk.out', 'yarn-error.log', 'coverage', '.DS_Store', '.idea', '.vs_code'];
 project.gitignore.exclude(...common_exclude);
 
 project.synth();
